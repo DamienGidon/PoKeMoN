4e0f414c08756cb9e201a2a30c24dd396c12ebda
Thibault Cam <thibault.cam1@yahoo.fr>
2018-01-26 14:09:54 +0100

    Add buttons

---
+++ app/src/main/java/org/esiea/cam_gidon/pokedex/DetailsActivity.java
@@ -0,0 +1,16 @@
+package org.esiea.cam_gidon.pokedex;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        String value = intent.getStringExtra("key");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
--- app/src/main/java/org/esiea/cam_gidon/pokedex/MainActivity.java
+++ app/src/main/java/org/esiea/cam_gidon/pokedex/MainActivity.java
@@ -1,7 +1,15 @@
 package org.esiea.cam_gidon.pokedex;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout ll = (RelativeLayout) findViewById(R.id.mainLayout);
+
+        for(int i = 1; i <= 151; i++){
+            Button button = new Button(this);
+            button.setText("Pokemon " + i);
+            button.setTextSize(20);
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+            params.setMargins(300, 300 * (i-1), 10, 10);
+            button.setLayoutParams(params);
+            button.setGravity(Gravity.CENTER);
+            button.setTag(i);
+            final int t = i;
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent myIntent = new Intent(MainActivity.this, DetailsActivity.class);
+                    myIntent.putExtra("key", t); //Optional parameters
+                    MainActivity.this.startActivity(myIntent);
+                }
+            });
+            ll.addView(button);
+        }
+    }
+
+    public void DetailsPage(View view) {
+        Intent myIntent = new Intent(MainActivity.this, DetailsActivity.class);
+        //myIntent.putExtra("key", value); //Optional parameters
+        MainActivity.this.startActivity(myIntent);
     }
 }
