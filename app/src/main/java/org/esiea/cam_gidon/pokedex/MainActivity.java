4d271f1fa7aa86b2bb4941882b3a7cbb4e5823a1
Thibault Cam <thibault.cam1@yahoo.fr>
2018-01-26 17:35:33 +0100

    des trucs en plus

--- app/src/main/java/org/esiea/cam_gidon/pokedex/MainActivity.java
+++ app/src/main/java/org/esiea/cam_gidon/pokedex/MainActivity.java
@@ -1,13 +1,19 @@
 package org.esiea.cam_gidon.pokedex;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import java.util.Locale;
 
@@ -18,8 +24,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RelativeLayout ll = (RelativeLayout) findViewById(R.id.mainLayout);
+        this.CreateButton();
+    }
 
+    public void CreateButton(){
+        RelativeLayout ll = (RelativeLayout) findViewById(R.id.mainLayout);
         for(int i = 1; i <= 151; i++){
             Button button = new Button(this);
             button.setText("Pokemon " + i);
@@ -35,17 +44,17 @@ public class MainActivity extends AppCompatActivity {
             button.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     Intent myIntent = new Intent(MainActivity.this, DetailsActivity.class);
-                    myIntent.putExtra("key", v.getTag().toString()); //Optional parameters
+                    myIntent.putExtra("key", v.getTag().toString());
+                    Toast.makeText(getApplicationContext(),getString(R.string.toast) + v.getTag().toString(), Toast.LENGTH_LONG).show();
                     MainActivity.this.startActivity(myIntent);
                 }
             });
             ll.addView(button);
         }
     }
-
-    public void DetailsPage(View view) {
-        Intent myIntent = new Intent(MainActivity.this, DetailsActivity.class);
-        //myIntent.putExtra("key", value); //Optional parameters
-        MainActivity.this.startActivity(myIntent);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
