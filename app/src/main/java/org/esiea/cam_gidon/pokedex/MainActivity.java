28d7338731de4cd993d9a8bb6729a31ac9e9da40
Damien G <damien.gidon@capgemini.com>
2018-01-29 11:59:17 +0100

    big commit of the world

--- app/src/main/java/org/esiea/cam_gidon/pokedex/MainActivity.java
+++ app/src/main/java/org/esiea/cam_gidon/pokedex/MainActivity.java
@@ -3,10 +3,14 @@ package org.esiea.cam_gidon.pokedex;
 import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,13 +19,29 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+    JSONArray pokemonList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.CreateButton();
+        this.getPokemon();
     }
 
     @Override
@@ -48,17 +68,19 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void CreateButton(){
+    public void CreateButton(JSONArray pokemonList){
         RelativeLayout ll = (RelativeLayout) findViewById(R.id.mainLayout);
-        for(int i = 1; i <= 151; i++){
+        for(int i = 0; i <= 150; i++){
+            JSONObject jsonobject = pokemonList.optJSONObject(i);
+            String name = jsonobject.optString("name");
             Button button = new Button(this);
-            button.setText("Pokemon " + i);
+            button.setText(name);
             button.setTextSize(20);
             RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
                     RelativeLayout.LayoutParams.WRAP_CONTENT,
                     RelativeLayout.LayoutParams.WRAP_CONTENT
             );
-            params.topMargin = 300*(i-1);
+            params.topMargin = 300*(i);
             params.addRule(RelativeLayout.CENTER_HORIZONTAL);
             button.setLayoutParams(params);
             button.setTag(i);
@@ -88,5 +110,32 @@ public class MainActivity extends AppCompatActivity {
             })
             .setIcon(android.R.drawable.ic_dialog_alert)
             .show();
+    }
+
+    private void getPokemon() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = "https://pokeapi.co/api/v2/pokemon?limit=151";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                if (response != null) {
+                    int resultCount = response.optInt("resultCount");
+
+                        pokemonList = response.optJSONArray("results");
+                        if (pokemonList != null) {
+                            Log.i("LOG", "test");
+                                }
+                    CreateButton( pokemonList );
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("LOG", error.toString());
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
     }
 }
